bfb33980ddf2d6e17d881a8c7952938b859c00f6
JeffreyCao <jeffreycao1024@gmail.com>
2020-03-08 22:57:14 +0800

    resize by percent

--- ezpp/resize.py
+++ ezpp/resize.py
@@ -6,11 +6,13 @@ import os
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter, ImageColor
 
 re_wh = re.compile(r'^([0-9]+)x([0-9]+)$')
+re_size = re.compile(r'^([0-9]+)$')
+re_percent = re.compile(r'^([0-9]+)%$')
 
 size_using = """
-The size shoud be [width]x[height] for example :300x400 . 
-When width == heigth just use size number directly .
-For example : 128 means a 128x128 size
+(1) WIDTH AND HEIGHT For example :300x400 ;
+(2) SIZE OF SQUARE For example : 128 means a 128x128 size ;
+(3) PERCENT For example : 25%% ;
 """
 
 
@@ -31,7 +33,8 @@ def create_cmd_parser(subparsers):
     resize_group.add_argument("-a",
                               "--app",
                               action='store_true',
-                              help=size_using)
+                              help="Resize app icons for android and ios")
+
     parser_resize.set_defaults(on_args_parsed=_on_args_parsed)
 
 
@@ -55,20 +58,49 @@ def _on_app_parsed(infile, outfile):
 
 
 def _on_size_parsed(infile, outfile, size):
+
+    (width, height) = _parse_wh_from_size(size)
+    new_width = width
+    new_height = height
+
+    img = Image.open(os.path.abspath(infile))
+    (origin_w, origin_h) = img.size
+
+    if width < 1 and height < 1:
+
+        new_width = int(origin_w * width)
+        new_height = int(origin_h * height)
+
+    bar_filename, ext = os.path.splitext(infile)
+    filename_new = outfile if outfile else f"{bar_filename}_{new_width}x{new_height}{ext}"
+
+    print(f"resize: ({origin_w}, {origin_h})->({new_width}, {new_height})")
+    print(f"from:   {os.path.abspath(infile)}")
+    print(f"to:     {os.path.abspath(filename_new)}")
+
+    img_tobe_scale = img.resize(
+        (int(new_width), int(new_height)), Image.ANTIALIAS)
+    img_tobe_scale.save(os.path.abspath(filename_new), 'PNG')
+
+
+def _parse_wh_from_size(size):
     m_wh = re_wh.match(size)
-    if not m_wh:
+    if m_wh:
+        width = int(m_wh.group(1))
+        height = int(m_wh.group(2))
+        return (width, height)
+
+    m_size = re_size.match(size)
+    if m_size:
+        width = int(m_size.group(1))
+        height = width
+        return (width, height)
+
+    m_percent = re_percent.match(size)
+    if m_percent:
+        width = float(m_percent.group(1))/float(100)
+        height = width
+        return (width, height)
+    else:
         print(size_using)
         exit(2)
-
-    width = m_wh.group(1)
-    height = m_wh.group(2)
-    bar_filename, ext = os.path.splitext(infile)
-    filename_new = outfile if outfile else f"{bar_filename}_{width}x{height}{ext}"
-    resize(infile, width, height, filename_new)
-
-
-def resize(filename, width, height, outputfile):
-    # print(f"{os.path.abspath(filename)} ->({width}, {height}) -> {os.path.abspath(outputfile)}")
-    img = Image.open(os.path.abspath(filename))
-    img_tobe_scale = img.resize((int(width), int(height)), Image.ANTIALIAS)
-    img_tobe_scale.save(os.path.abspath(outputfile), 'PNG')
