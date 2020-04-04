1929e460bd93977fd1021fe68e512b5beb7661fe
JeffreyCao <jeffreycao1024@gmail.com>
2020-04-04 23:53:31 +0800

    0.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 setuptools.setup(
     name="ezpp",
-    version="0.0.8",
+    version="0.0.9",
     keywords=("pip", "ezpp"),
     description="Easy to process picturse",
     long_description="Easy to process picturse",
