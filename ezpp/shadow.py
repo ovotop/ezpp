a7409806e0741295fd24dbd79347098fb7f45fbe
JeffreyCao <jeffreycao1024@gmail.com>
2020-08-09 14:49:00 +0800

    fix dark line from (0,0)

--- ezpp/shadow.py
+++ ezpp/shadow.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-from . import global_args
-import re
 import os
+import re
+import sys
 import argparse
+from PIL import Image, ImageColor
+from ezutils.colors import *
+from . import global_args
+
+COLOR_SAME_RADUS = 160
 
 
 def create_cmd_parser(subparsers):
     cmd_parser = subparsers.add_parser(
-        'shadow', help='shadow help')
-    cmd_parser.add_argument("-s",
-                            "--size",
-                            help="argument help")
+        'shadow', help='Add long shadow on a image which has clean background')
+    cmd_parser.add_argument("-a",
+                            "--alpha",
+                            help="(0.0,1.0] 0.0 None shadow 1.0 black shadow")
     cmd_parser.set_defaults(on_args_parsed=_on_args_parsed)
 
     return cmd_parser
@@ -20,24 +25,81 @@ def _on_args_parsed(args):
     params = vars(args)
     infile, outfile, recursive = global_args.parser_io_argments(params)
 
-    sizeStr = params['size']
-    if not sizeStr:
-        sizeStr = '10'
-
-    shadow(infile, outfile, recursive, sizeStr)
-
-
-def shadow_file(infile, outfile, blurSize):
-    pass
+    alphaStr = params['alpha']
+    if not alphaStr:
+        alphaStr = '0.5'
+    alpha = float(alphaStr)
+    shadow(infile, outfile, recursive, alpha)
 
 
-def shadow(infile, outfile, recursive, sizeStr=10):
+def sameColor(colorA, colorB):
+    r = colorA[0] - colorB[0]
+    g = colorA[1] - colorB[1]
+    b = colorA[2] - colorB[2]
+    return r*r+g*g+b*b < COLOR_SAME_RADUS*COLOR_SAME_RADUS
+
+
+def shadowColor(colorbg, alpha):
+    r, g, b = colorbg
+    h, s, v = rgb2hsv(r, g, b)
+    r1, g1, b1 = hsv2rgb(h, s, (1-alpha)*v)
+    strHex = bytearray([r1, g1, b1]).hex()
+    return f"#{strHex}"
+
+
+def shadowOnline(pixes, width, height, fromX, fromY, alpha):
+    first = None
+    firstObj = None
+    for i in range(0, width):
+        x = fromX+i
+        y = fromY+i
+        if x >= width or y >= height:
+            continue
+
+        if first == None:
+            first = pixes[x, y]
+            continue
+
+        if firstObj == None:
+            if not sameColor(first, pixes[x, y]):
+                firstObj = pixes[x, y]
+            continue
+
+        if sameColor(first, pixes[x, y]):
+            pixes[x, y] = ImageColor.getcolor(
+                shadowColor(pixes[x, y], alpha), "RGB")
+
+
+def shadowOnPixes(pixes, width, height, alpha):
+    for i in range(0, width):
+        shadowOnline(pixes, width, height, i, 0, alpha)
+
+    for j in range(1, height):
+        shadowOnline(pixes, width, height, 0, j, alpha)
+
+
+def shadowOnImage(img, alpha):
+    imgSize = img.size
+    pixes = img.load()
+    width, height = imgSize
+    shadowOnPixes(pixes, width, height, alpha)
+
+
+def shadow_file(fileName, outFile, alpha):
+    print(f'shadow file with alpha= {alpha}:\n{fileName} \n to {outFile}')
+    with Image.open(fileName) as im:
+        shadowOnImage(im, alpha)
+        im.show()
+        im.save(outFile)
+
+
+def shadow(infile, outfile, recursive, alpha=0.5):
 
     if recursive == None or recursive == False:
-        return shadow_file(infile, outfile, sizeStr)
+        return shadow_file(infile, outfile, alpha)
 
     infiles = global_args.get_recursive_pic_infiles(infile)
     for infile_for_recursive in infiles:
         shadow_file(infile_for_recursive,
                     infile_for_recursive,
-                    sizeStr)
+                    alpha)
