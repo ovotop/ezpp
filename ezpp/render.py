04a88a776bb0eefb4f1e0161ad2e3372ed7d460c
JeffreyCao <jeffreycao1024@gmail.com>
2020-08-24 01:15:43 +0800

    antialias_size

--- ezpp/render.py
+++ ezpp/render.py
@@ -36,19 +36,19 @@ def _on_args_parsed(args):
     render(infile, outfile, params_map, preview)
 
 
-def render_canvas_file(infile, params_map):
+def render_canvas_file(infile, params_map, antialias_size=1):
     data_str = readstr(infile)
     infile_dir, infile_name = os.path.split(infile)
     yaml_cfg = yaml.load(merge_params(data_str, params_map),
                          Loader=yaml.FullLoader)
-    return render_canvas(yaml_cfg, infile_dir, params_map)
+    return render_canvas(yaml_cfg, infile_dir, params_map, antialias_parent=antialias_size)
 
 
-def render_canvas(yaml_cfg, infile_dir, params_map):
+def render_canvas(yaml_cfg, infile_dir, params_map, antialias_parent=1):
     width = int(_.get(yaml_cfg, 'canvas.width'))
     height = int(_.get(yaml_cfg, 'canvas.height'))
-    antialias_size = int(_.get(yaml_cfg, 'canvas.antialias_size', '1'))
-
+    cfg_antialias_size = int(_.get(yaml_cfg, 'canvas.antialias_size', '1'))
+    antialias_size = cfg_antialias_size * antialias_parent
     # canvas
     color = _.get(yaml_cfg, 'canvas.color')
     if color == None:
@@ -57,46 +57,49 @@ def render_canvas(yaml_cfg, infile_dir, params_map):
                              height*antialias_size), color)
 
     # items
-    img_layers = Image.new(
+    img_items = Image.new(
         'RGBA', (width*antialias_size, height*antialias_size), ("#0000"))
     items = _.get(yaml_cfg, 'items')
-    for layer in items:
-        render_layer(img_layers, layer, infile_dir, params_map)
+    for item in items:
+        render_item(img_items, item, infile_dir, params_map,
+                    antialias_size=antialias_size)
 
-    img.paste(img_layers, (0, 0), mask=img_layers)
-
+    img.paste(img_items, (0, 0), mask=img_items)
     if antialias_size > 1:
-        img = img.resize((width, height), Image.ANTIALIAS)
+        resize_width = int(width*antialias_parent)
+        resize_height = int(height * antialias_parent)
+        img = img.resize((resize_width, resize_height), Image.ANTIALIAS)
     return img
 
 
-def render_layer(img, layer, infile_dir, params_map):
-    layer_type = _.get(layer, 'type')
-    if layer_type == "image":
-        render_image_layer(img, layer, infile_dir)
-    elif layer_type == "text":
-        render_text_layer(img, layer, infile_dir)
-    elif layer_type == "shadow":
-        render_shadow_layer(img, layer)
-    elif layer_type == "import":
-        render_import_layer(img, layer, infile_dir, params_map)
-    elif layer_type == "nested":
-        render_nested_layer(img, layer, infile_dir, params_map)
+def render_item(img, item, infile_dir, params_map, antialias_size=1):
+    item_type = _.get(item, 'type')
+    if item_type == "image":
+        render_image_item(img, item, infile_dir, antialias_size)
+    elif item_type == "text":
+        render_text_item(img, item, infile_dir, antialias_size=antialias_size)
+    elif item_type == "shadow":
+        render_shadow_item(img, item)
+    elif item_type == "import":
+        render_import_item(img, item, infile_dir, params_map, antialias_size)
+    elif item_type == "nested":
+        render_nested_item(img, item, infile_dir, params_map, antialias_size)
 
 
-def render_image_layer(img, layer, infile_dir):
-    file_name = _.get(layer, 'filename')
+def render_image_item(img, item, infile_dir, antialias_size=1):
+    file_name = _.get(item, 'filename')
     layer_img = Image.open(os.path.join(infile_dir, file_name)).convert("RGBA")
-    paste_layer_img(img, layer, layer_img, infile_dir)
+    paste_item_img(img, item, layer_img, infile_dir, antialias_size)
 
 
-def paste_layer_img(img, layer, layer_img, infile_dir):
-    x = _.get(layer, 'pos.x')
-    y = _.get(layer, 'pos.y')
+def paste_item_img(img, item, layer_img, infile_dir, antialias_size=1):
+    posx = _.get(item, 'pos.x')
+    x = (posx * antialias_size) if type(posx) != type("str") else posx
+    posy = _.get(item, 'pos.y')
+    y = (posy * antialias_size) if type(posy) != type("str") else posy
     w, h = img.size
 
     layer_w, layer_h = layer_img.size
-
     if x == "center":
         x = int((w-layer_w)/2)
 
@@ -105,20 +108,23 @@ def paste_layer_img(img, layer, layer_img, infile_dir):
     img.paste(layer_img, (x, y), mask=layer_img)
 
 
-def render_text_layer(img, layer, infile_dir):
-    title = _.get(layer, 'title')
-    font_size = _.get(layer, 'font.size')
-    font_filename = _.get(layer, 'font.filename')
-    font_filepath = _.get(layer, 'font.path')
+def render_text_item(img, item, infile_dir, antialias_size=1):
+    title = _.get(item, 'title')
+    font_size = _.get(item, 'font.size')
+    font_filename = _.get(item, 'font.filename')
+    font_filepath = _.get(item, 'font.path')
     font_path = font_filepath if font_filepath != None else os.path.join(
         infile_dir, font_filename)
-    color = _.get(layer, 'font.color')
+    color = _.get(item, 'font.color')
     font = ImageFont.truetype(
         font_path,
-        font_size
+        font_size * antialias_size
     )
-    x = _.get(layer, 'pos.x')
-    y = _.get(layer, 'pos.y')
+    posx = _.get(item, 'pos.x')
+    x = (posx * antialias_size) if type(posx) != type("str") else posx
+    posy = _.get(item, 'pos.y')
+    y = (posy * antialias_size) if type(posy) != type("str") else posy
+
     w, h = img.size
     if x == "center" and y == "center":
         text_center(title, color, font, img, w, h)
@@ -131,8 +137,8 @@ def render_text_layer(img, layer, infile_dir):
         draw.text((x, y), title, color, font=font)
 
 
-def render_shadow_layer(img, layer):
-    alpha = _.get(layer, 'alpha')
+def render_shadow_item(img, item):
+    alpha = _.get(item, 'alpha')
     shadow_on_image(img, alpha)
 
 
@@ -141,18 +147,21 @@ def merge_dicts(dict1, dict2):
     return res
 
 
-def render_import_layer(img, layer, infile_dir, params_map):
-    filename = _.get(layer, 'filename')
-    params = _.get(layer, 'params')
+def render_import_item(img, item, infile_dir, params_map, antialias_size=1):
+    filename = _.get(item, 'filename')
+    params = _.get(item, 'params')
     new_params = merge_dicts(params_map, params) if params else params_map
     layer_img = render_canvas_file(
-        os.path.join(infile_dir, filename), new_params)
-    paste_layer_img(img, layer, layer_img, infile_dir)
+        os.path.join(infile_dir, filename),
+        new_params,
+        antialias_size)
+    paste_item_img(img, item, layer_img, infile_dir, antialias_size)
 
 
-def render_nested_layer(img, layer, infile_dir, params_map):
-    layer_img = render_canvas(layer, infile_dir, params_map)
-    paste_layer_img(img, layer, layer_img, infile_dir)
+def render_nested_item(img, item, infile_dir, params_map, antialias_size=1):
+    layer_img = render_canvas(item, infile_dir, params_map,
+                              antialias_parent=antialias_size)
+    paste_item_img(img, item, layer_img, infile_dir, antialias_size)
 
 
 def merge_params(data_str, params):
