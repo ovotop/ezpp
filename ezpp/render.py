d7b55a0b8e402e930e6a003327ef35f0e5463bef
JeffreyCao <jeffreycao1024@gmail.com>
2020-08-30 16:47:10 +0800

    add item visible

--- ezpp/render.py
+++ ezpp/render.py
@@ -72,7 +72,13 @@ def render_canvas(yaml_cfg, infile_dir, params_map, antialias_parent=1):
 
 
 def render_item(img, item, infile_dir, params_map, antialias_size=1):
+    item_visible = _.get(item, 'visible', True)
+    print("render_item.visible", item_visible, item)
+    if not item_visible:
+        return
+
     item_type = _.get(item, 'type')
+
     if item_type == "image":
         render_image_item(img, item, infile_dir, antialias_size)
     elif item_type == "text":
