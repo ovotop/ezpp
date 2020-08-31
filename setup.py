ddf5d381115ab81d07d2d87d3d14c266843372d5
曹建峰 <caojianfeng@innobuddy.com>
2020-08-31 10:46:39 +0800

    0.2.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name="ezpp",
-    version="0.2.1",
+    version="0.2.2",
     keywords=["pip", "ezpp", "resize", "reformat",
               "recolor", "shadow", "icon", "logo",
               "yaml render", "photoshop", "ps"],
