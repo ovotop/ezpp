664fa2b751788a8b680c6db8704da5eac0858673
JeffreyCao <jeffreycao1024@gmail.com>
2020-09-11 21:06:28 +0800

    test freetype

--- test_freetype.py
+++ test_freetype.py
@@ -1,4 +1,6 @@
-import freetype
+import freetype  # pip3 install freetype-py
+
+MAX_CHARS_COUNT = 512
 
 
 def charinfo2char(charinfo):
@@ -6,10 +8,24 @@ def charinfo2char(charinfo):
     return chr(charcode)
 
 
-face = freetype.Face("/System/Library/fonts/Menlo.ttc")
-print(face.num_charmaps)
-chars = list(face.get_chars())[:350]
-chars_str = list(map(charinfo2char, chars))
-# for charcode, index in chars:
-#     print(chr(charcode))
-print(''.join(chars_str))
+# face = freetype.Face("/System/Library/fonts/Menlo.ttc")
+# face = freetype.Face("/System/Library/fonts/Symbol.ttf")
+# face = freetype.Face("/System/Library/fonts/Apple Braille Outline 6 Dot.ttf")
+# face = freetype.Face("/System/Library/fonts/ZapfDingbats.ttf")
+# face = freetype.Face("/System/Library/fonts/Apple Color Emoji.ttc")
+# face = freetype.Face("/System/Library/fonts/ヒラギノ明朝 ProN.ttc")
+
+def fontchars(fontpath):
+    face = freetype.Face("/System/Library/fonts/Supplemental/NISC18030.ttf")
+    chars = list(face.get_chars())
+    charscount = len(chars)
+    print(f'chars({charscount})')
+    chars_str = list(map(charinfo2char, chars))[:MAX_CHARS_COUNT]
+    for charcode, index in chars:
+        print(charcode)
+    print(''.join(chars_str))
+    if charscount > MAX_CHARS_COUNT:
+        print('...')
+
+if __name__ == "__main__":
+    print(fontchars("/System/Library/fonts/Supplemental/NISC18030.ttf"))
